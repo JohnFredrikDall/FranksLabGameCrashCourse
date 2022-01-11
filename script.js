03e3c0d0742e048c462d3ee22350aab114ada046
JohnFredrikDall <joffe.dall@gmail.com>
2022-01-11 10:23:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const canvas = /** @type {HTMLCanvasElement} */(document.getElementById('canvas1'));
+const ctx = /** @type {HTMLCanvasElement} */(canvas.getContext('2d'));
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Cat {
+    constructor(){
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.directionX = Math.random() * 5 - 3;
+        this.directionY = Math.random() * 5 - 2.5;
+    }
+    
+    update(){
+        this.x -= this.directionX;
+    }
+
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    }
+}
