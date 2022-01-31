cfed6b11d8dceda0ee9368ebf1ff21fd6efcf764
eivindskandsen <eivindskandsen@hotmail.no>
2022-01-31 10:25:37 +0100

    push-not-organized

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
 export default class Background{
 
-constructor(ctx, canvas){
-    this.backgroundLayer=new Image()
-    
-    this.ctx=ctx;
-}
+    constructor(ctx, canvas){
+        this.backgroundLayer=new Image()
+        
+        this.ctx=ctx;
+    }
 
-getImage(backgroundSRC){
-    this.backgroundLayer.src=backgroundSRC;
-}
+    getImage(backgroundSRC){
+        this.backgroundLayer.src=backgroundSRC;
+    }
 
-drawBackground(width, height){
-    this.ctx.drawImage(this.backgroundLayer,0,0,width, height)
-}
+    drawBackground(x,y,width, height){
+        this.ctx.drawImage(this.backgroundLayer,x,y,width, height)
+    }
+
+    update(x,x2, canvasBGm1, gamespeed){
+    }
 }
