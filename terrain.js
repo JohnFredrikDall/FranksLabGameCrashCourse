493aa1024bc60b59fab95420b695da9409c7d15f
Martin <martindaa@hotmail.no>
2022-01-31 10:37:02 +0100

    terrain_design

--- terrain.js
+++ terrain.js
@@ -4,10 +4,16 @@ export default class Terrain {
         this.height = 50;
         this.x = 100;
         this.y = 900;
-        // this.y = canvas.height - 100;
+        this.canvas = canvas;
         this.directionX = Math.random() * 1 + 0;
         this.directionY = Math.random() * 5 - 2.5;
         this.ctx = ctx;
+        this.terrainImage = new Image();
+        this.terrainImage.src = 'sprites/twilight-tiles.png';
+        this.spriteWidth = 47;
+        this.spriteHeight = 12;
+        this.spriteCoordinates = {x: 1, y:1}
+
     } 
     
     update(){
@@ -16,7 +22,8 @@ export default class Terrain {
     }
 
     draw(){
-        this.ctx.fillStyle = 'Black';
+        this.ctx.fillStyle = 'rgba(0, 0, 0, 0)';
         this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.ctx.drawImage(this.terrainImage, this.spriteCoordinates.x, this.spriteCoordinates.y, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
