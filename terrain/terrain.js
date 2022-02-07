7f41679f407052b2a886d1efe52d8990151e2db3
Martin <martindaa@hotmail.no>
2022-02-07 11:26:23 +0100

    some changes

--- terrain/terrain.js
+++ terrain/terrain.js
@@ -1,19 +1,16 @@
 export default class Terrain {
     constructor(ctx, canvas){
-        this.width = 300;
-        this.height = 50;
         this.x = 100;
         this.y = 900;
+        this.width = 300;
+        this.height = 50;
         this.canvas = canvas;
-        this.directionX = Math.random() * 1 + 0;
-        this.directionY = Math.random() * 5 - 2.5;
         this.ctx = ctx;
         this.terrainImage = new Image();
         this.terrainImage.src = 'sprites/twilight-tiles.png';
-        this.spriteWidth = 47;
-        this.spriteHeight = 12;
-        this.spriteCoordinates = {x: 1, y:1}
-
+        this.spriteCoordinates = {x: 0, y: 0, spriteWidth: 47, spriteHeight: 13}
+        //this.spriteCoordinates2 = {x: 66, y: 16, spriteWidth: 44, spriteHeight: 11} 
+        
     } 
     
     update(){
@@ -24,6 +21,7 @@ export default class Terrain {
     draw(){
         this.ctx.fillStyle = 'rgba(0, 0, 0, 0)';
         this.ctx.fillRect(this.x, this.y, this.width, this.height);
-        this.ctx.drawImage(this.terrainImage, this.spriteCoordinates.x, this.spriteCoordinates.y, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        this.ctx.drawImage(this.terrainImage, this.spriteCoordinates.x, this.spriteCoordinates.y, this.spriteCoordinates.spriteWidth, this.spriteCoordinates.spriteHeight, this.x, this.y, this.width, this.height);
     }
+   
 }
